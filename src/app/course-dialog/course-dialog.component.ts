754161708aa7ea9f929fae8bf94fa02a3a0dc334
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-12 18:15:19 +0100

    Refactor and clean code

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -38,23 +38,16 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
     }
 
     ngOnInit() {
-      /*
-      // Detect value changes in the form
-      const formChanges$ =
-        this.form.valueChanges
-        .pipe(
-            // Filter values that are invalid
-            filter( () => this.form.valid )
-          );
-      formChanges$.subscribe(console.log);
-      */
-     this.form.valueChanges
-     .pipe(
-         // Filter values that are invalid
-         filter( () => this.form.valid )
-       )
-      .subscribe(
+
+      const formChanges$ = this.form.valueChanges
+      .pipe(
+          // Filter values that are invalid
+          filter( () => this.form.valid )
+        );
+
+      formChanges$.subscribe(
         changes => {
+          // It's necessary mixing mapping and concatenation
           const saveCourse$ = fromPromise( this.saveCourse(changes));
           saveCourse$.subscribe(console.log);
         }
