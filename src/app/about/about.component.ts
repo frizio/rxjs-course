5935e8f803c0739fc59b7745bd7fc593a02d2f11
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-12 13:32:54 +0100

    SUbscription behaviours: error and complete

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -15,16 +15,28 @@ export class AboutComponent implements OnInit {
     // Stream DEFINITION (not instance) with RxJS: A stream is of type Observable<>
     const interval$ = interval(1000);
 
+    // An observable is a blue print for a stream.
     // An observable become a stream only if there is a subscription to them.
-    // interval$.subscribe( (value) => console.log('Stream 1: ' + value) );
+    interval$.subscribe( (value) => console.log('Stream 1: ' + value) );
     // Another stream
-    // interval$.subscribe( (value) => console.log('Stream 2: ' + value) );
+    interval$.subscribe( (value) => console.log('Stream 2: ' + value) );
 
     const timer$ = timer(3000, 1000);
     timer$.subscribe( (value) => console.log('Stream 3: ' + value) );
 
     const click$ = fromEvent(document, 'click');
     click$.subscribe( (event) => console.log(event) );
+
+    const theSubscription = interval$.subscribe(
+      (value) => console.log('Stream 5: ' + value),
+      (error) => console.log(error),
+      () => console.log('Stream 5 completed')
+    );
+
+    setTimeout(() => {
+      console.log('Unsubscription stream 5');
+      theSubscription.unsubscribe();
+    }, 5000);
 
   }
 
