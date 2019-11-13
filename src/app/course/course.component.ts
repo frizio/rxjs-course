1150abd8a17d6cdb30d1e5c088921588abd28ae4
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-13 11:39:57 +0100

    Format code

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,54 +1,44 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
 import {
-    debounceTime,
-    distinctUntilChanged,
-    startWith,
-    tap,
-    delay,
-    map,
-    concatMap,
-    switchMap,
-    withLatestFrom,
-    concatAll, shareReplay
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../model/course';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  startWith,
+  tap,
+  delay,
+  map,
+  concatMap,
+  switchMap,
+  withLatestFrom,
+  concatAll,
+  shareReplay
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
-import {Lesson} from '../model/lesson';
-
+import { merge, fromEvent, Observable, concat } from 'rxjs';
+import { Lesson } from '../model/lesson';
 
 @Component({
-    selector: 'course',
-    templateUrl: './course.component.html',
-    styleUrls: ['./course.component.css']
+  selector: 'course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('searchInput', { static: true }) input: ElementRef;
 
 
-    @ViewChild('searchInput', { static: true }) input: ElementRef;
+  constructor(private route: ActivatedRoute) {}
 
-    constructor(private route: ActivatedRoute) {
+  ngOnInit() {
+    const courseId = this.route.snapshot.params['id'];
+  }
 
-
-    }
-
-    ngOnInit() {
-
-        const courseId = this.route.snapshot.params['id'];
-
-
-
-    }
-
-    ngAfterViewInit() {
-
-
-
-
-    }
-
-
-
+  ngAfterViewInit() {}
 
 }
