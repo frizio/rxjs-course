8881335851f42acd98b4688b6570e38af838f121
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-13 15:48:56 +0100

    Fix code

--- server/get-courses.route.ts
+++ server/get-courses.route.ts
@@ -13,14 +13,12 @@ export function getAllCourses(req: Request, res: Response) {
         console.log('ERROR loading courses!');
         res.status(500).json({message: 'random error occurred.'});
     } else {
+      setTimeout(() => {
+          res.status(200).json({payload: Object.values(COURSES)});
+          // res.status(500).json({message: 'Error occurred.'});
+      }, 200);
 
-    setTimeout(() => {
-
-        res.status(200).json({payload: Object.values(COURSES)});
-        // res.status(500).json({message: 'Error occurred.'});
-    }, 200);
-
-   // }
+   }
 }
 
 
