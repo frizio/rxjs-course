831cd48aa347aa903683ceffa0cda2bca4103f98
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-13 15:46:22 +0100

    Modify getAllCourses server method

--- server/get-courses.route.ts
+++ server/get-courses.route.ts
@@ -1,26 +1,23 @@
 
 
 import {Request, Response} from 'express';
-import {COURSES} from "./db-data";
+import {COURSES} from './db-data';
 
 
 
 export function getAllCourses(req: Request, res: Response) {
 
-    /*
     const error = (Math.random() >= 0.5);
 
     if (error) {
-        console.log("ERROR loading courses!");
+        console.log('ERROR loading courses!');
         res.status(500).json({message: 'random error occurred.'});
-    }
-    else {
-    */
+    } else {
 
     setTimeout(() => {
 
-        res.status(200).json({payload:Object.values(COURSES)});
-
+        res.status(200).json({payload: Object.values(COURSES)});
+        // res.status(500).json({message: 'Error occurred.'});
     }, 200);
 
    // }
@@ -29,11 +26,11 @@ export function getAllCourses(req: Request, res: Response) {
 
 export function getCourseById(req: Request, res: Response) {
 
-    const courseId = req.params["id"];
+    const courseId = req.params['id'];
 
-    const courses:any = Object.values(COURSES);
+    const courses: any = Object.values(COURSES);
 
-    const course = courses.find(course => course.id == courseId);
+    const course = courses.find(course => course.id === courseId);
 
     res.status(200).json(course);
 }
