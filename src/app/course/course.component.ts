2f53121f2f977727276054401b7eb7f2ec2579d2
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-13 11:52:13 +0100

    Setting Up the Course Component

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -22,6 +22,7 @@ import {
 } from 'rxjs/operators';
 import { merge, fromEvent, Observable, concat } from 'rxjs';
 import { Lesson } from '../model/lesson';
+import { createHttpObservable } from '../common/util';
 
 @Component({
   selector: 'course',
@@ -30,6 +31,9 @@ import { Lesson } from '../model/lesson';
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
+  course$: Observable<Course>;
+  lessons$: Observable<Lesson>;
+
   @ViewChild('searchInput', { static: true }) input: ElementRef;
 
 
@@ -37,6 +41,13 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     const courseId = this.route.snapshot.params['id'];
+
+    this.course$ = createHttpObservable(`/api/courses/${courseId}`);
+    this.lessons$ = createHttpObservable(`/api/lessons?courseId=${courseId}&pageSize=100`)
+                      .pipe(
+                        map( res => res['payload'] )
+                      );
+
   }
 
   ngAfterViewInit() {}
